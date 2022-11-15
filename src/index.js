593b39f46aeb8cd18df505ad7c76e64e0be57f39
binhtran21rew <102853177+binhtran21rew@users.noreply.github.com>
2022-11-15 21:29:51 +0700

    add

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const morgan = require('morgan');
+const {engine} = require('express-handlebars');
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+// app use 
+app.use(morgan('combined'));
+app.engine('hbs', engine({extname: '.hbs'}))
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources','views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.listen(port , () => console.log(`app listening http://localhost:${port}`))
