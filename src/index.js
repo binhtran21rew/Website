58b43318676abe013631f148cc7c751b027e59cf
binhtran21rew <102853177+binhtran21rew@users.noreply.github.com>
2022-11-16 21:16:07 +0700

    t

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const path = require('path');
+const path = require   ('path');
 const app = express();
 const morgan = require('morgan');
-const {engine} = require('express-handlebars');
+const {engine  } = require('express-handlebars');
 const port = 3000;
 
 
